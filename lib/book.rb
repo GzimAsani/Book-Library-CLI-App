a98c43b53f217ad80d0cc7130c827b836ebc7902
Gzim <gzim.asani@gig-ext.com>
2025-06-28 14:42:32 +0200

    Add file structure and implemented the book class

---
+++ lib/book.rb
@@ -0,0 +1,30 @@
+class Book
+  attr_accessor :title, :author, :genre
+
+  def initialize(title:, author:, genre:)
+    @title = title
+    @author = author
+    @genre = genre
+  end
+
+  def to_hash
+    {
+      title: title,
+      author: author,
+      genre: genre
+    }
+  end
+
+  def self.from_hash(hash)
+    # p "test #{hash}"
+    new(title: hash["title"], author: hash["author"], genre: ["genre"])
+  end
+
+end
+
+hash = { "title" => "The Hobbit", "author" => "J.R.R. Tolkien", "genre" => "Fantasy" }
+
+
+p Book.from_hash(hash)
+
+
